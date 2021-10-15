2c65c1d13b140412332fdc0cd813110dd4736d92
Christian M. Adams <chadams@redhat.com>
2021-10-15 16:05:45 -0400

    Add script to inject custom annotations from base template
    Signed-off-by: Christian M. Adams <chadams@redhat.com>

---
+++ config/manifests/bases/inject-custom-config.py
@@ -0,0 +1,22 @@
+'''
+After generating the bundle, inject custom configuration such as
+annotations, OLM parameters, etc.
+'''
+
+import yaml
+
+annotations_path = "../../../bundle/metadata/annotations.yaml"
+existing_annotations = open(annotations_path, 'r')
+annotations = yaml.safe_load(existing_annotations)
+
+
+raw_annotations = open("annotations.yaml")
+base_annotations = yaml.safe_load(raw_annotations)
+
+# Inject custom annotations
+annotations['annotations'].update(base_annotations['annotations'])
+
+file_content = yaml.safe_dump(annotations, default_flow_style=False, explicit_start=True)
+
+with open(annotations_path, 'w') as f:
+    f.write(file_content)
